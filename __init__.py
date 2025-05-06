8c8eebe52ef7578dd150446ebe48feed131e762e
Locked-chess-official <13140752715@163.com>
2025-05-06 23:14:33 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,8 +3,11 @@ r"""
 #endif
 
 #include <string>
+#include <iostream>
 
 std::string combine(std::string cpp_code, std::string py_code, bool use_double=true) {
+	if (cpp_code.find("\'\'\'") || cpp_code.find("\"\"\"")) std::cerr << "SyntaxWarning: the code may be wrong due to the \'\'\' or \"\"\" in cpp_code" << std::endl;
+	if (py_code.find("#endif")) std::cerr << "SyntaxWarning: the code may be wrong due to \'#endif\' in py_code" << std::endl;
 	if (use_double) return "#if false\nr\"\"\"\n#endif\n" + cpp_code + "\n#if false\n\"\"\"\n" + py_code + "\n#endif";
 	return "#if false\nr\'\'\'\n#endif\n" + cpp_code + "\n#if false\n\'\'\'\n" + py_code + "\n#endif";
 }
